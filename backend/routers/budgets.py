c93c644aab8b2f3fc94f88887dcc91585937adbc
ll-ohan <lohan.lacroix@icloud.com>
2026-01-21 14:39:24 +0100

    [TDD] Code Ajouter Budget - Route API - Erreur 400

--- backend/routers/budgets.py
+++ backend/routers/budgets.py
@@ -12,13 +12,15 @@ router = APIRouter(
 @router.post("/", response_model=BudgetRead, status_code=status.HTTP_201_CREATED)
 def create_budget(budget: BudgetCreate, db: Session = Depends(get_db)):
 
-    service = BudgetService(db)
+    try :
+        service = BudgetService(db)
 
-    nouveau_budget = service.add_budget(
-        categorie_id=budget.categorie_id,
-        montant=budget.montant,
-        date_debut=budget.date_debut,
-        date_fin=budget.date_fin
-    )
-    return nouveau_budget
-   
+        nouveau_budget = service.add_budget(
+            categorie_id=budget.categorie_id,
+            montant=budget.montant,
+            date_debut=budget.date_debut,
+            date_fin=budget.date_fin
+        )
+        return nouveau_budget
+    except ValueError as ve:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(ve))
