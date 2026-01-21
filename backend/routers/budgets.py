0f1808aa4aa033e107d28d0c4e833466453ba92e
ll-ohan <lohan.lacroix@icloud.com>
2026-01-21 14:13:26 +0100

    [TDD] Code Ajouter Budget - Route API

---
+++ backend/routers/budgets.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database import get_db
+from scripts.saisie_budget import BudgetService 
+from schemas.budget import BudgetCreate, BudgetRead
+
+router = APIRouter(
+    prefix="/api/budgets",
+    tags=["budgets"]
+)
+
+@router.post("/", response_model=BudgetRead, status_code=status.HTTP_201_CREATED)
+def create_budget(budget: BudgetCreate, db: Session = Depends(get_db)):
+
+    service = BudgetService(db)
+
+    nouveau_budget = service.add_budget(
+        categorie_id=budget.categorie_id,
+        montant=budget.montant,
+        date_debut=budget.date_debut,
+        date_fin=budget.date_fin
+    )
+    return nouveau_budget
+   
